51b9785316fa13997eb722ad8573c2d1c3b2acad
Manshu-PC <onemint@gmail.com>
2019-07-12 14:42:33 -0400

    Hello World!

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,11 @@
 
-
+//a simple program that sends out a greeting to the world
 public class MyClass
 {
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+    
     
 }
