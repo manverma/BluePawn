95fcb84b8da3d5859813fc5eb8fe3c36ba6559ad
manverma <manverma@LNAR-PF12126N.corp.capgemini.com>
2019-07-30 17:09:37 -0400

    Added Method to print Namaste.

---
+++ src/MyFirstMethods.java
@@ -0,0 +1,20 @@
+
+//a simple program that sends out a greeting to the world
+public class MyFirstMethods
+{
+    public static void main (String[] args)
+    {
+    	myFirstMethod();
+    	mySecondMethod();
+    }
+    
+    public static void myFirstMethod() {
+    	
+    	System.out.println("Namaste World!");
+
+    }
+    
+    public static void mySecondMethod() {
+    	System.out.println("Shukriya");
+    }
+}
